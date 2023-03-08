95e2cc7ec6a953519e98e94cd9af6b8994174b37
unknown <Riyanagwl@gmail.com>
2023-03-08 16:53:53 +0300

    update with rails console

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :usebelongs_to :author, class_name: "User"
+  belongs_to :author, class_name: "User"
   belongs_to :post
-
   def update_post_comments_counter
-  post.update(comments_count: post.comments.count)
-end
+    post.update(comments_count: post.comments.count)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_000000) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_162248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_000000) do
     t.bigint "author_id", null: false
     t.string "title"
     t.string "text"
-    t.integer "comments_count"
-    t.integer "likes_count"
+    t.integer "comments_count", default: 0
+    t.integer "likes_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_000000) do
     t.string "name"
     t.string "photo"
     t.string "bio"
-    t.integer "posts_count"
+    t.integer "posts_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
