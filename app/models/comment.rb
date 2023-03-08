3a3bab69a6f430e5844500aafc583a9d64037f07
unknown <Riyanagwl@gmail.com>
2023-03-08 15:29:30 +0300

    Add custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :usebelongs_to :author, class_name: "User"
   belongs_to :post
+
+  def update_post_comments_counter
+  post.update(comments_count: post.comments.count)
+end
 end
