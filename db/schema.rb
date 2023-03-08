20f3e07cca5c49a8129671807f050f190311aa0a
unknown <Riyanagwl@gmail.com>
2023-03-08 12:06:37 +0300

    update scheme

--- db/schema.rb
+++ db/schema.rb
@@ -15,33 +15,33 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_000000) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "Author_Id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["AuthorId"], name: "index_comments_on_AuthorId"
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "Author_Id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["AuthorId"], name: "index_likes_on_AuthorId"
+    t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint "Author_Id", null: false
+    t.bigint "author_id", null: false
     t.string "title"
     t.string "text"
     t.integer "comments_count"
     t.integer "likes_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["AuthorId"], name: "index_posts_on_AuthorId"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
