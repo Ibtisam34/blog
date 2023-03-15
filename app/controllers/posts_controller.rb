c3fea049ad79abc12692d2be00c9ea0b649246f4
unknown <Riyanagwl@gmail.com>
2023-03-15 17:22:25 +0300

    update posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(post_params.merge! (likes_counter: 0 , comments_counter: 0))
     @post.author_id = current_user.id
     if @post.save
       redirect_to "/users/#{current_user.id}/posts"
